e4067777947e166b456c1c84de1b9f8b8cc1ad25
crickclash <crickclashgamingsolutions@gmail.com>
2026-03-12 14:21:04 +0530

    mwrge website crud

--- app/Models/KeyInstruction.php
+++ app/Models/KeyInstruction.php
@@ -11,7 +11,7 @@ class KeyInstruction extends Model
 
     protected $connection = 'admin_panel';
 
-    protected $table = 'key_instructions';
+    protected $table = 'key_instrctions';
 
     protected $fillable = [
         'icon',

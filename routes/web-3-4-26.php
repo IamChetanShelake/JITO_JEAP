479e1fdb9487641715f3bd7855054a88dbd07594
crickclash <crickclashgamingsolutions@gmail.com>
2026-04-03 11:18:42 +0530

    test

--- routes/web-3-4-26.php
+++ routes/web-3-4-26.php
@@ -161,7 +161,7 @@ Route::middleware(['admin', 'auth.active'])->prefix('admin')->name('admin.')->gr
     // Working Committee Routes
     Route::resource('committee', WorkingCommitteeController::class);
 
-    // Zone Routes  
+    // Zone Routes
     Route::resource('zones', ZoneController::class);
 
     // Website Management Routes
@@ -232,7 +232,7 @@ Route::middleware(['admin', 'auth.active'])->prefix('admin')->name('admin.')->gr
     Route::delete('/website/about/testimonials-success/{id}', [AdminController::class, 'deleteTestimonialsSuccess'])->name('website.about.testimonials-success.delete');
 
     Route::get('/website/application', [AdminController::class, 'websiteApplication'])->name('website.application');
-    
+
     // Application Sub-Pages - FAQs
     Route::get('/website/application/faqs', [AdminController::class, 'websiteApplicationFaqs'])->name('website.application.faqs');
     Route::post('/website/application/faqs/store', [AdminController::class, 'storeFaq'])->name('website.application.faqs.store');
@@ -328,9 +328,6 @@ Route::middleware(['admin', 'auth.active'])->prefix('admin')->name('admin.')->gr
         ->name('third_stage_documents.approve');
     Route::post('/third-stage-documents/user/{user}/send-back', [AdminController::class, 'sendBackThirdStageDocument'])
         ->name('third_stage_documents.sendback');
-        
-    Route::get('/third-stage-documents/user/{user}/generate-pdf', [AdminController::class, 'generateThirdStageDocumentPDF'])
-            ->name('third_stage_documents.generate_pdf');
 
 
     Route::get('/chapters/resubmit', [AdminController::class, 'chapterResubmit'])->name('chapter.resubmit');
@@ -566,15 +563,11 @@ Route::middleware(['auth', 'user'])
         // Generate Summary PDF
         Route::get('/{user}/generate-summary-pdf', [AdminController::class, 'generateSummaryPDF'])->name('generate.summary.pdf');
 
-        // View Sanction Letter
-        Route::get('/{user}/sanction-letter', [AdminController::class, 'viewSanctionLetter'])->name('sanction.letter');
-        
         Route::get('/user/{user}/generate-short-summary-pdf', [AdminController::class, 'generateShortSummaryPDF'])->name('user.generate.shortsummary.pdf');
 
         Route::get('/financial-closure/{user}', [AdminController::class, 'generateFinancialClosurePDF'])->name('user.generate.financial_closure.pdf');
-        
-        Route::get('/third-stage-documents/user/{user}/generate-pdf', [AdminController::class, 'generateThirdStageDocumentPDF'])
-            ->name('third_stage_documents.generate_pdf');
+        // View Sanction Letter
+        Route::get('/{user}/sanction-letter', [AdminController::class, 'viewSanctionLetter'])->name('sanction.letter');
 
         // Route for above 1 lakh application redirection
         Route::get('/above-1-lakh-application', [UserController::class, 'above1LakhApplication'])
